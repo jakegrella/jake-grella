8ddc5e960c8a9f6a4b673c580f88f286492c4e53
Jake Grella <jake@jakegrella.com>
2022-03-02 21:40:02 -0600

    site

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import avatar from './avatar.png';
+import picchu from './picchu.jpeg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <img
+          className='avatar'
+          src={avatar}
+          alt='avatar of jake grella wearing a green beanie'
+        />
+        <h1>jake grella</h1>
       </header>
+      <main>
+        <img className='picchu' src={picchu} alt='machu picchu, 1911' />
+        <div className='content'>
+          <p>
+            <a
+              href='https://twitter.com/jakegrella'
+              target='_blank'
+              rel='noreferrer'>
+              @jakegrella
+            </a>{' '}
+            /{' '}
+            <a
+              href='mailto:jake@jakegrella.com'
+              target='_blank'
+              rel='noreferrer'>
+              jake@jakegrella.com
+            </a>
+          </p>
+          <h2>🧢</h2>
+          <ul>
+            <li>northwestern mutual: july 2021 - present</li>
+            <li>contractor: may - july 2021</li>
+          </ul>
+          <h2>🪚</h2>
+          <ul>
+            <li>
+              <a
+                href='https://headedwesttt.com'
+                target='_blank'
+                rel='noreferrer'>
+                headedwesttt.com:
+              </a>{' '}
+              making real estate content businesses more efficient
+            </li>
+            <li>
+              <a
+                href='https://whalewatching.xyz'
+                target='_blank'
+                rel='noreferrer'>
+                whalewatching.xyz:
+              </a>{' '}
+              view the largest crypto transactions
+            </li>
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
